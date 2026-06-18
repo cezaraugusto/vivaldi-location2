87a2db4d6c69a4e5443dcf1445bee813ae848e17
Cezar Augusto <boss@cezaraugusto.net>
2026-06-17 23:46:27 -0300

    Apply eslint-config-auditor formatting and lint rules

--- bin.js
+++ bin.js
@@ -1,9 +1,8 @@
-#!/usr/bin/env node
 
 const locateVivaldi =
-  require('./dist/index.cjs').default || require('./dist/index.cjs');
+  require('./dist/index.cjs').default || require('./dist/index.cjs')
 
-const argv = process.argv.slice(2);
-const allowFallback = argv.includes('--fallback') || argv.includes('-f');
+const argv = process.argv.slice(2)
+const allowFallback = argv.includes('--fallback') || argv.includes('-f')
 
-console.log(locateVivaldi(allowFallback));
+console.log(locateVivaldi(allowFallback))
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,11 @@
-import { expect, test, describe } from 'vitest';
-import vivaldiLocation from '../src/index';
+import {expect, test, describe} from 'vitest'
+
+import vivaldiLocation from '../src/index'
 
 describe('vivaldi-location2 module', () => {
-  test('returns string or null', () => {
-    const res = vivaldiLocation();
-    expect(typeof res === 'string' || res === null).toBe(true);
-  });
-});
+  it('returns string or null', () => {
+    const res = vivaldiLocation()
+
+    expect(typeof res === 'string' || res === null).toBe(true)
+  })
+})
